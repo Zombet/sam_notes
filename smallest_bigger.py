8e46b3dab34bdd4fde90212433c1d61e1c67ccb1
Ifrah <sadiq21ifrah@gmail.com>
2025-02-25 23:10:31 +0530

    changed

--- smallest_bigger.py
+++ smallest_bigger.py
@@ -1,4 +1,4 @@
-'''import itertools
+import itertools
 
 def generate_permutations(arr):
     return sorted(list(itertools.permutations(arr)))
@@ -15,6 +15,6 @@ a=permutations.index(tuple(arr))+1
 if a>=len(permutations):
     print("Not possible")
 else:
-    print("".join(list(permutations[a])))'''
+    print("".join(list(permutations[a])))
 
 
